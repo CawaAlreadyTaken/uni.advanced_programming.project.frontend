7537021a4f9a2c2a248949a6a190f4353447acdf
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2025-01-29 11:38:24 +0100

    We want to parametrize frontend

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,4 @@
-//export const API_PORT = import.meta.env.VITE_API_PORT || "3000";
-export const API_PORT = "2000";
+export const NODE_ID = Number(import.meta.env.VITE_NODE_ID);
+export const API_PORT = 2000+NODE_ID;
 export const API_BASE_URL = `http://localhost:${API_PORT}`;
+export const FRONTEND_PORT = Number(import.meta.env.VITE_PORT);
